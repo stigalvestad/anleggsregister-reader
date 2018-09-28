9470f7985b46cf5007a7b13b9a39fae88e1e8c4d
Stig A <stigalvestad@gmail.com>
2018-09-28 11:54:03 +0200

    Was able to filter and present relevant maps for Grane

--- src/read_data.py
+++ src/read_data.py
@@ -1,6 +1,12 @@
 import json
 import pandas as pd
+import sys
 from pprint import pprint
+
+
+def set_encoding():
+    reload(sys) # just to be sure
+    sys.setdefaultencoding('utf-8')
 
 
 def get_data_frame_from_json_file(rel_file_path):
@@ -16,10 +22,25 @@ def get_data_frame_from_json_file(rel_file_path):
     # pprint(search_results)
 
     res = json.dumps(search_results, ensure_ascii=False)
-    print res
+    # print res
 
     return pd.read_json(res, orient='records')
 
 
-df = get_data_frame_from_json_file('../data/anlegg.json')
-print df
+set_encoding()
+df = get_data_frame_from_json_file('../data/anlegg2.json')
+# print df
+
+# list column names
+print list(df)
+
+# list unique owners
+# print df.ownerName.unique()
+df_grane = df[(df.ownerName == 'IK GRANE ARENDAL ORIENTERING') | (df.ownerName == 'GRANE IDRETTSLAG')]
+print df_grane
+# 'IK GRANE ARENDAL ORIENTERING' u'GRANE IDRETTSLAG'
+# 'AUSTRE MOLAND IDRETTSLAG'
+
+# print df.operatorName.unique()
+df_grane_short = df_grane[['buildingYear', 'classDescription', 'facilityId', 'name', 'ownerName', 'ownerOrgno', 'status', 'totalGrants', 'totalPayments', 'totalRevoked', 'typeDescription']]
+print df_grane_short
